83a2f65bba89aa924f13177ca8b24a96516b0dcc
Wilkins Jardines <wjard001@fiu.edu>
2023-10-24 22:05:05 -0400

    mysql database and menu, booking models added

--- workspace/LittleLemonProject/restaurant/models.py
+++ workspace/LittleLemonProject/restaurant/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Menu(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    date = models.DateField()
+    time = models.TimeField()
+    number_of_guests = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
--- workspace/LittleLemonProject/settings.py
+++ workspace/LittleLemonProject/settings.py
@@ -73,12 +73,16 @@ WSGI_APPLICATION = 'LittleLemonProject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+# superuser credentials username: admin1 password: Miami001
+DATABASES = { 'default': { 
+ 'ENGINE': 'django.db.backends.mysql',
+ 'NAME': 'mydatabase',
+ 'USER': 'newuser1',
+ 'PASSWORD': 'Miami001!',
+ 'HOST': '127.0.0.1',
+ 'PORT': '3306',
+ 'OPTIONS': {
+ 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'" } }
 }
 
 
